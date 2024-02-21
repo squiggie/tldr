a50fbf8d77f57d0a51b22ef17c97aac829e1d3ea
Landen <squigge@gmail.com>
2024-02-21 09:25:52 -0600

    Add dynamic article refresh

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,23 @@
-from flask import render_template
+from flask import render_template, jsonify, request
 from .models import Article
 
 def init_app(app):
     @app.route("/", methods=("GET", "POST"))
     def index():
-        articles = Article.query.all()
-        print(articles)
+        # Retrieve the latest 100 articles from the database
+        articles = Article.query.order_by(Article.added_date.desc()).limit(100).all()
         return render_template('index.html', articles=articles)
+    
+    @app.route("/articles/newest/", methods=("GET",))
+    def newest_articles():
+        guid = request.args.get('guid')
+        article = Article.query.get(guid)
+        last_date = article.added_date
+        articles = Article.query.filter(Article.added_date > last_date).all()
+        dict = [article.as_dict() for article in articles]
+        query = Article.query.filter(Article.added_date > last_date)
+        print(f"Last date: {last_date}")
+        print(str(query))  # or print(query.statement) for the full query
+        return jsonify(dict)
+
+            
--- app/static/main.js
+++ app/static/main.js
@@ -31,4 +31,63 @@ $(document).ready( function () {
 
         })
     })
+
+    setInterval(updateArticles, 60000);
+
+    async function updateArticles() {
+        const articleCards = document.querySelectorAll('#articles-container .card');
+        let latestArticleCard = null;
+        let latestTimestamp = 0;
+        
+        articleCards.forEach(card => {
+            const addedDate = new Date(card.getAttribute('data-added-date'));
+            if (addedDate > latestTimestamp) {
+                latestTimestamp = addedDate;
+                latestArticleCard = card;
+            }
+        });
+        
+        // Query the API for new articles since the last one
+        const response = await fetch(`/articles/newest?guid=${latestArticleCard.getAttribute('data-guid')}`);
+        const data = await response.json();
+        
+        // Append the new articles to the page
+        data.forEach(article => {
+          const articleHTML = `
+            <div class="card m1" data-guid="${article.guid}" data-added-date="${article.added_date}">
+                <div class="card-content">
+                    <div class="media">
+                        <!-- Image on the left -->
+                        <div class="media-left">
+                            <figure class="image is-96x96"> <!-- Adjust size as needed -->
+                                <img src="${article.image}" alt="Article image" style="object-fit: cover;">
+                            </figure>
+                        </div>
+                        <!-- Content on the right -->
+                        <div class="media-content">
+                            <p class="title is-4">"${article.title}" </p>
+                            <p class="subtitle is-6">by:"${article.author}"</p>
+                        </div>
+                    </div>
+
+                    <div class="content">
+                        "${article.synopsis}"
+                        <br>
+                        Published: <time datetime="${article.published_date}">${article.published_date}</time>
+                    </div>
+                </div>
+                    <footer class="card-footer">
+                        <a href="${article.url}"  target= "_blank" class="card-footer-item">Read More</a>
+                        <a href="#" class="card-footer-item">Save</a>
+                        <a href="#" class="card-footer-item">Share</a>
+                    </footer>
+                </div>`;
+            // Prepend the new article HTML at the top of the container
+            if (container.firstElementChild) {
+                container.firstElementChild.insertAdjacentHTML('beforebegin', articleHTML);
+            } else {
+                container.innerHTML += articleHTML;
+            }
+        });
+      }
 })
