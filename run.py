50b0de57687f84098c789c544fcfa0fd9a18390b
Landen <squigge@gmail.com>
2024-02-26 16:36:31 -0600

    remove dependencies

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 from app import create_app
 from flask_apscheduler import APScheduler
-from scripts.fetcharticles import fetch_and_store_articles
-from scripts.summarizearticles import process_queue
 
 app = create_app()
 
