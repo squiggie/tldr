ff4a29bc29ef7009b09ef608d7f07c556b5208e2
Landen <squigge@gmail.com>
2024-02-28 13:55:45 -0600

    add category page and links

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 from app import create_app
-from flask_apscheduler import APScheduler
-
+from app.models import Category
 app = create_app()
+
+@app.context_processor
+def inject_categories():
+    categories = Category.query.all()
+    return dict(categories=categories)
 
 if __name__ == '__main__':
     app.run(debug=True)
